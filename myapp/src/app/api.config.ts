11bc16749b4e80254684e41568c829cff487e8cc
Idanbenaim <idanbenaim@gmail.com>
2023-09-23 16:55:15 +0300

    updating render domain in api config file

--- myapp/src/app/api.config.ts
+++ myapp/src/app/api.config.ts
@@ -1,4 +1,4 @@
 
 
-export const BASE_API_URL = 'http://localhost:8000';
-
+// export const BASE_API_URL = 'http://localhost:8000';
+export const BASE_API_URL = 'https://io-records-back.onrender.com';
